7bfbcb86231cef26dc5d40623b7b28b002521d2a
Kuanl Bisht <kuanl15bisht@gmail.com>
2025-03-31 14:57:40 +0530

    added views for register and login.

--- Bus/reservation/views.py
+++ Bus/reservation/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from reservation import models
 
-# Create your views here.
+def register(request):
+    if request.method =="POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        user_list = models.User.objects.filter(username = username)
+        error_mssg =[]
+        if user_list:
+            error_mssg = "User name is already exist"
+            return render(request, "register.html", {"error_mssg":error_mssg})
+        else:
+            username = models.Relog.objects.create(username = username, email = email, password = password)
+            username.save()
+            return redirect("login")
+    return render(request, "register.html")
+
+def login(request):
+    if request.method =="POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        obj_user = models.Relog.objects.filter(username=username, password=password)
+
+        if obj_user:
+            return redirect('dashboard')
+        
+        error_mssg = "User not found"
+        return render(request, "login.html", {"error_mssg":error_mssg})
+    return render(request, "login.html")
+
