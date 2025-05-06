f54b94548141a1524082ec99d83c4f7a2ea209b2
Kuanl Bisht <kuanl15bisht@gmail.com>
2025-05-06 15:44:32 +0530

    code updated.

--- Bus/static/js/book.js
+++ Bus/static/js/book.js
@@ -7,15 +7,4 @@ const numberOfPersons = parseInt(document.getElementById("No_of_person").value) 
 
 const totalAmount = price * numberOfPersons;
 document.getElementById("amount").value = totalAmount.toFixed(2); // show 2 decimal places
-console.log(document.getElementById("amount").value)
-console.log(document.getElementById("user").value)
-console.log(document.getElementById("bus_name").value)
-console.log(document.getElementById("from").value)
-console.log(document.getElementById("date").value)
-console.log(document.getElementById("price").value)
-console.log(document.getElementById("email").value)
-console.log(document.getElementById("bus_number").value)
-console.log(document.getElementById("to").value)
-console.log(document.getElementById("No_of_person").value)
-console.log(document.getElementById("time").value)
 }
